1f44e43cb58885429795bfa7fe99b2a99cdf7bf5
matsulenko <matsulenko@mail.ru>
2024-01-31 18:16:42 +0300

    Initial Commit

---
+++ Stopwatches/StopwatchesApp.swift
@@ -0,0 +1,17 @@
+//
+//  StopwatchesApp.swift
+//  Stopwatches
+//
+//  Created by Matsulenko on 31.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct StopwatchesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
